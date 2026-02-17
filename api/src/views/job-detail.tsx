1468267a829e52bf98c87ce7a0a80da515654a40
Uncle Bike <adamtilley.is@gmail.com>
2026-02-17 18:37:43 -0500

    fix: mobile job footer overlap

--- api/src/views/job-detail.tsx
+++ api/src/views/job-detail.tsx
@@ -249,7 +249,7 @@ export const JobDetailPage = ({ job, customer, service, territory, team, assigne
         </div>
       </div>
 
-      <div class="p-4 sm:p-8" style="padding-bottom: calc(84px + var(--safe-bottom));">
+      <div class="p-4 sm:p-8" style="padding-bottom: calc(104px + var(--safe-bottom));">
         <div class="mx-auto" style="max-width: 1120px;">
           <div class="grid gap-4 lg:grid-cols-[1fr,320px] lg:gap-6">
             <div class="grid gap-4 sm:gap-6">
@@ -593,6 +593,8 @@ export const JobDetailPage = ({ job, customer, service, territory, team, assigne
               </div>
             </aside>
           </div>
+
+          <div class="sm:hidden" aria-hidden="true" style="height: calc(76px + var(--safe-bottom));"></div>
         </div>
       </div>
 
